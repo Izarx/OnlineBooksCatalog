345fc3236fa6ce31c721917df9cc2c49174ec91f
Ihor Zakharko <izakh@softserveinc.com>
2021-10-07 17:42:04 +0300

    fix errors in configuration, set 9 tomcat and 11 java

--- BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/controller/HomeController.java
+++ BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.softserveinc.booklibrary.controller;
 
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
+@RestController
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
+	@GetMapping(value="/test")
+	public String test() {
+		return "home";
 	}
 }
