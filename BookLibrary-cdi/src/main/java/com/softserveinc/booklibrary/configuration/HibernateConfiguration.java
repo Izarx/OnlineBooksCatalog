9fd7ba4b93d54daad24085e42d3d76e02e1e8c9c
Ihor Zakharko <izakh@softserveinc.com>
2021-10-13 19:58:56 +0300

    add parameters into HibernateConfiguration by property file

--- BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/configuration/HibernateConfiguration.java
+++ BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/configuration/HibernateConfiguration.java
@@ -6,8 +6,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -16,14 +19,18 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@PropertySource("classpath:persistence-postgresql.properties")
 @EnableTransactionManagement
 public class HibernateConfiguration {
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
 		entityManagerFactoryBean.setDataSource(dataSource);
-		entityManagerFactoryBean.setPackagesToScan("com.softserveinc.booklibrary.entity");
+		entityManagerFactoryBean.setPackagesToScan(env.getProperty("hibernate.package"));
 		entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 		entityManagerFactoryBean.setJpaProperties(hibernateProperties());
 		return entityManagerFactoryBean;
@@ -32,11 +39,10 @@ public class HibernateConfiguration {
 	@Bean
 	public DataSource dataSource() {
 		HikariDataSource dataSource = new HikariDataSource();
-		//TODO these properties should be extracted to a property file
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/booklibrary");
-		dataSource.setUsername("booklibrary");
-		dataSource.setPassword("Qwerty12");
+		dataSource.setDriverClassName(env.getProperty("hibernate.driver"));
+		dataSource.setJdbcUrl(env.getProperty("hibernate.jdbcurl"));
+		dataSource.setUsername(env.getProperty("hibernate.username"));
+		dataSource.setPassword(env.getProperty("hibernate.password"));
 		dataSource.setMaximumPoolSize(5);
 		return dataSource;
 	}
@@ -55,9 +61,8 @@ public class HibernateConfiguration {
 
 	private Properties hibernateProperties() {
 		Properties hibernateProperties = new Properties();
-		//TODO extract to a property file
-		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
-		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		hibernateProperties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
 		return hibernateProperties;
 	}
 }
--- BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/entity/Review.java
+++ BookLibrary-cdi/src/main/java/com/softserveinc/booklibrary/entity/Review.java
@@ -2,7 +2,6 @@ package com.softserveinc.booklibrary.entity;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
