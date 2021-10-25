e81e8e9be5b3575cc47f1c005f3014839eb03afe
Ihor Zakharko <igor.zaharko@gmail.com>
2021-10-25 13:03:03 +0300

    Add styles from template (#23)
    Styles from template were added

--- BookLibrary-cdi/frontend/src/app/app-routing.module.ts
+++ BookLibrary-cdi/frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {AuthorsComponent} from "./authors/authors.component";
 import {BooksComponent} from "./books/books.component";
+import {ReviewsComponent} from "./reviews/reviews.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'authors', component: AuthorsComponent},
-    {path: 'books', component: BooksComponent}
+    {path: 'books', component: BooksComponent},
+    {path: 'reviews', component: ReviewsComponent}
 ]
 
 @NgModule({
