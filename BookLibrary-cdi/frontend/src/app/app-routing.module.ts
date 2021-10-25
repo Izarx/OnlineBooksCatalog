1ffb66665d21846cc04266cbd30c5e0238d93be9
Ihor Zakharko <igor.zaharko@gmail.com>
2021-10-25 10:24:32 +0300

    Add clean angular template (#22)
    Directory with angular project added
    Basic components created
    Basic routes created

---
+++ BookLibrary-cdi/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {AuthorsComponent} from "./authors/authors.component";
+import {BooksComponent} from "./books/books.component";
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'authors', component: AuthorsComponent},
+    {path: 'books', component: BooksComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- BookLibrary-cdi/frontend/src/app/app.module.ts
+++ BookLibrary-cdi/frontend/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { AuthorComponent } from './author/author.component';
+import { ReviewComponent } from './review/review.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    AuthorComponent,
+    ReviewComponent,
+    AuthorsComponent,
+    BooksComponent,
+    ReviewsComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
