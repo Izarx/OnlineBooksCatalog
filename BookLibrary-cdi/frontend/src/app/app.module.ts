b72041ef9d4efab343bad6df4e43b9065a345150
Ihor Zakharko <igor.zaharko@gmail.com>
2021-10-27 11:28:56 +0300

    remove unused components

--- BookLibrary-cdi/frontend/src/app/app.module.ts
+++ BookLibrary-cdi/frontend/src/app/app.module.ts
@@ -1,23 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
-import { AuthorComponent } from './author/author.component';
-import { ReviewComponent } from './review/review.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { BooksComponent } from './books/books.component';
-import { ReviewsComponent } from './reviews/reviews.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {AuthorsComponent} from './authors/authors.component';
+import {BooksComponent} from './books/books.component';
+import {ReviewsComponent} from './reviews/reviews.component';
+import {HomeComponent} from './home/home.component';
 import {FormsModule} from "@angular/forms";
 import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    AuthorComponent,
-    ReviewComponent,
     AuthorsComponent,
     BooksComponent,
     ReviewsComponent,
