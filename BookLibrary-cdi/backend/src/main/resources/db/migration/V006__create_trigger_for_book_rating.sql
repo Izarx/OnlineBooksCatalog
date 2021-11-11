b402f52a7b718dd27f3d67a8598616838e5d9070
Ihor Zakharko <igor.zaharko@gmail.com>
2021-11-11 11:33:50 +0200

    add triggers for author and book rating update

--- BookLibrary-cdi/backend/src/main/resources/db/migration/V006__create_trigger_for_book_rating.sql
+++ BookLibrary-cdi/backend/src/main/resources/db/migration/V006__create_trigger_for_book_rating.sql
@@ -7,12 +7,12 @@ CREATE OR REPLACE FUNCTION books_calculate_rating()
         old_rating NUMERIC(3, 2);
     BEGIN
         IF (tg_op = 'INSERT') THEN
-	        count_ratings := (SELECT count(review_id) FROM reviews WHERE book_id = new.book_id);
+	        count_ratings := (SELECT count(review_id) FROM reviews WHERE reviews.book_id = new.book_id group by reviews.book_id);
 	        old_rating := (SELECT book_rating FROM books WHERE book_id = new.book_id);
-	        new_rating := (old_rating * count_ratings + new.rating)/(count_ratings + 1);
+	        new_rating := (old_rating * (count_ratings - 1) + new.rating)/(count_ratings);
 	        UPDATE books SET book_rating = new_rating WHERE book_id = new.book_id;
 	    ELSEIF (tg_op = 'DELETE') THEN
-		    RAISE EXCEPTION 'Delete review forbidden : % -> %',
+		    RAISE EXCEPTION 'Delete review forbidden : %',
 			    old;
         ELSEIF (tg_op = 'UPDATE') THEN
 			RAISE EXCEPTION 'Change review forbidden : % -> %',
@@ -24,5 +24,7 @@ CREATE OR REPLACE FUNCTION books_calculate_rating()
 	LANGUAGE 'plpgsql';
 
 CREATE TRIGGER books_rating
-    AFTER INSERT OR UPDATE OR DELETE ON reviews
-    FOR EACH ROW EXECUTE PROCEDURE books_calculate_rating();
+	AFTER INSERT OR DELETE OR UPDATE OF rating
+	ON public.reviews
+	FOR EACH ROW
+EXECUTE FUNCTION public.books_calculate_rating();
