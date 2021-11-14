5a68a338c2204b65411d2b2c7257e37b586d4cc4
Ihor Zakharko <igor.zaharko@gmail.com>
2021-11-14 21:44:41 +0200

    add triggers for author and book rating update second version

--- BookLibrary-cdi/backend/src/main/java/com/softserveinc/booklibrary/backend/controller/AuthorController.java
+++ BookLibrary-cdi/backend/src/main/java/com/softserveinc/booklibrary/backend/controller/AuthorController.java
@@ -9,6 +9,7 @@ import com.softserveinc.booklibrary.backend.entity.Author;
 import com.softserveinc.booklibrary.backend.service.AuthorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- BookLibrary-cdi/backend/src/main/resources/db/migration/V006__create_trigger_for_book_rating.sql
+++ BookLibrary-cdi/backend/src/main/resources/db/migration/V006__create_trigger_for_book_rating.sql
@@ -1,30 +1,32 @@
-CREATE OR REPLACE FUNCTION books_calculate_rating()
+CREATE OR REPLACE FUNCTION calculate_rating()
  RETURNS TRIGGER AS
-    $books_calculate_rating$
-    DECLARE
-        count_ratings INTEGER;
-        new_rating NUMERIC(3, 2);
-        old_rating NUMERIC(3, 2);
+    $calculate_rating$
     BEGIN
-        IF (tg_op = 'INSERT') THEN
-	        count_ratings := (SELECT count(review_id) FROM reviews WHERE reviews.book_id = new.book_id group by reviews.book_id);
-	        old_rating := (SELECT book_rating FROM books WHERE book_id = new.book_id);
-	        new_rating := (old_rating * (count_ratings - 1) + new.rating)/(count_ratings);
-	        UPDATE books SET book_rating = new_rating WHERE book_id = new.book_id;
-	    ELSEIF (tg_op = 'DELETE') THEN
-		    RAISE EXCEPTION 'Delete review forbidden : %',
-			    old;
-        ELSEIF (tg_op = 'UPDATE') THEN
-			RAISE EXCEPTION 'Change review forbidden : % -> %',
-				old, new;
-	    END IF;
+	    update books b set book_rating = (
+	        select avg(rating) from reviews r where r.book_id = b.book_id
+		    )
+	    WHERE b.book_id = new.book_id;
+
+	    update authors a set author_rating = (
+	        select avg(book_rating)
+	        from books join authors_books ab on ab.book_id = books.book_id
+	        where ab.author_id = a.author_id
+		    )
+	    from (
+		    select authors.author_id
+		    from authors
+			join authors_books on authors_books.author_id = authors.author_id
+		    where authors_books.book_id = new.book_id
+		    ) as aabai
+	    where aabai.author_id = a.author_id;
+
     RETURN new;
     END;
-    $books_calculate_rating$
+    $calculate_rating$
 	LANGUAGE 'plpgsql';
 
 CREATE TRIGGER books_rating
-	AFTER INSERT OR DELETE OR UPDATE OF rating
+	AFTER INSERT
 	ON public.reviews
 	FOR EACH ROW
-EXECUTE FUNCTION public.books_calculate_rating();
+EXECUTE FUNCTION public.calculate_rating();
